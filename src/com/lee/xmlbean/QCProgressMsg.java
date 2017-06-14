688582af6bf3670545f1d6ddc4a08d7c2c5766b2
master <759353118@qq.com>
2017-06-14 15:28:57 +0800

    xml parse

---
+++ src/com/lee/xmlbean/QCProgressMsg.java
@@ -0,0 +1,24 @@
+package com.lee.xmlbean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by lijie on 2017/6/13.
+ */
+public class QCProgressMsg {
+    private List<QCProgress> qcProgresses;
+    public QCProgressMsg() {
+        qcProgresses = new ArrayList<>();
+    }
+
+    public List<QCProgress> getQcProgresss() {
+        return qcProgresses;
+    }
+
+    public void setQcProgresss(List<QCProgress> qcProgresses) {
+        for (Object qcProgress : qcProgresses) {
+            this.qcProgresses.add((QCProgress) qcProgress);
+        }
+    }
+}
