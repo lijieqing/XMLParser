4611c45e143c3f3f7e7256ebd41208f839497a57
lijieqing <759353118@qq.com>
2017-12-20 15:15:59 +0800

    XMLParse API注释修改 结构优化

--- src/com/lee/api/XMLAPI.java
+++ src/com/lee/api/XMLAPI.java
@@ -1,13 +1,12 @@
 package com.lee.api;
 
 import com.lee.xml.*;
-import com.lee.xmlbean.*;
 import org.dom4j.Attribute;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
-import java.io.FileInputStream;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
@@ -23,12 +22,12 @@ public class XMLAPI {
     //返回 xmlbean 包下的根目录
 
     /**
-     * Read xml object.
+     * 将XML文件读取为XML标签对象
      *
      * @param inputStream the input stream of xml file
      * @return the object
      */
-    public static Object readXML(InputStream inputStream){
+    public static Object readXML(InputStream inputStream) {
         Element rootElement = null;
         SAXReader reader = new SAXReader();
         try {
@@ -37,33 +36,45 @@ public class XMLAPI {
             e.printStackTrace();
         }
         //先 将root 节点解析出来
-        if (document != null)rootElement = document.getRootElement();
+        if (document != null) rootElement = document.getRootElement();
         XMLBase root = new XMLHasKids(rootElement.getName());
         List<Attribute> attributes = rootElement.attributes();
         List<XMLAttribute> xmlAttributes = new ArrayList<>();
         for (Attribute attribute : attributes) {
-            xmlAttributes.add(new XMLAttribute(attribute.getName(),attribute.getValue()));
+            xmlAttributes.add(new XMLAttribute(attribute.getName(), attribute.getValue()));
         }
         root.setXMLAttributes(xmlAttributes);
 
         //利用递归 将子节点逐一解析 放入xmlbase 中
         List<Element> childelement = rootElement.elements();
         for (Element element : childelement) {
-            XmlReader.XMLparse(element,root);
+            XmlReader.XMLparse(element, root);
         }
 
         return root.transform();
     }
 
-    public static void writeObj2Xml(Object object,String path) throws IOException, IllegalAccessException, InvocationTargetException {
-//        InputStream inputStream = new FileInputStream("D:/temp.xml");
-//        Object device =  XMLAPI.readXML(inputStream);
-        XmlGenerate.generate(object,path);
+    /**
+     * 将XML标签对象输出为XML文件
+     *
+     * @param object
+     * @param path
+     * @throws IOException
+     * @throws IllegalAccessException
+     * @throws InvocationTargetException
+     */
+    public static void writeObj2Xml(Object object, String path) throws IOException, IllegalAccessException, InvocationTargetException {
+        XmlGenerate.generate(object, path);
     }
 
-    public static void changPackageName(String pacakagename){
-        if (pacakagename != null && !pacakagename.trim().equals("")){
-            Globals.CLASSNAME = pacakagename+".";
+    /**
+     * 更改XML标签类的包路径
+     *
+     * @param pacakagename
+     */
+    public static void changPackageName(String pacakagename) {
+        if (pacakagename != null && !pacakagename.trim().equals("")) {
+            Globals.CLASSNAME = pacakagename + ".";
         }
     }
 
