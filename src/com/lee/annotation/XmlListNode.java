a08488e16aaffdebd0e69aef4fa8ef765a3c36ae
lijieqing <759353118@qq.com>
2018-02-23 11:44:19 +0800

    XML注解功能修改，对于集合标签需要指定标签名称

--- src/com/lee/annotation/XmlListNode.java
+++ src/com/lee/annotation/XmlListNode.java
@@ -12,7 +12,7 @@ import java.lang.annotation.Target;
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface XmlListNode {
-    String name() default "";
+    String name();
 
     Class<?> nodeType();
 }
